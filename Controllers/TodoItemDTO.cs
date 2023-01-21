74b21c52bd764520d84a5fa3ff14db63c57d608e
TharushiJayawardena <tharushijayawardena03@gmail.com>
2023-01-21 16:40:13 +0530

    add API project

---
+++ Controllers/TodoItemDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoAPI.Controllers
+{
+    public class TodoItemDTO
+    {
+        public bool IsComplete { get; internal set; }
+        public string Name { get; internal set; }
+        public long Id { get; internal set; }
+    }
+}
